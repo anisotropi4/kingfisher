9d0ee0aee9b72426da3b9f465b5c0fbc89f27ed6
Will Deakin <will.deakin@crinstitute.org.uk>
2024-02-19 07:26:58 +0000

    FY 2018-2021 and thread

--- output-crs.py
+++ output-crs.py
@@ -2,6 +2,8 @@
 """output-crs: generate per station PNG files"""
 
 import os
+from functools import partial
+from multiprocessing import Pool, Process
 
 import geopandas as gp
 import matplotlib.pyplot as plt
@@ -59,8 +61,10 @@ def get_gb():
     beeching = mainland.buffer(9000.0)
     beeching = Polygon(unary_union(beeching))
     beeching = gp.GeoSeries(beeching, crs=CRS)
-    write_dataframe(beeching.to_frame("geometry"), "ine.gpkg", layer="beeching")
-    station = read_dataframe("work/odm-path.gpkg", layer="station_point")
+    write_dataframe(
+        beeching.to_frame("geometry"), "work/beeching.gpkg", layer="beeching"
+    )
+    station = read_dataframe("work/odm-station.gpkg", layer="odm_station")
     beeching_label = (
         """Aberdeen,Barrow-In-Furness,Birmingham,Blackpool,Bournmouth,Brighton,Bristol,Cambridge,"""
         """Cardiff,Carlisle,Derby,Dover,Dundee,Edingburgh,Exeter,Glasgow,Gloucester,Grimsby,"""
@@ -77,36 +81,56 @@ def get_gb():
     map_label = station.set_index("CRS").loc[beeching_crs, ["Name", "geometry"]]
     map_label["label"] = beeching_label
     map_label = map_label.reset_index()
-    write_dataframe(map_label, "data/ine.gpkg", layer="label")
+    write_dataframe(map_label, "work/beeching.gpkg", layer="label")
 
 
-def main():
-    """main"""
-    mainland = read_dataframe("data/ine.gpkg", layer="mainland")
-    for filename in sorted(os.listdir("output")):
-        crs = filename.replace(".gpkg", "")
-        filepath = f"image/{crs[0]}/{crs}-rail.png"
+def get_filepath(image, crs, year):
+    """get_filepath: vetor SVG or image PNG"""
+    if image == "vector":
+        return f"vector/{crs[0]}/{crs}-{year}-rail.svg"
+    if image == "image":
+        return f"image/{crs[0]}/{crs}-{year}-rail.png"
+    return f"image/{crs[0]}/{crs}-{year}-rail.png"
+
+
+def write_image(filename, image="image"):
+    """write_image: output image file"""
+    crs = filename.replace(".gpkg", "")
+    try:
+        gf = read_dataframe(f"output/{filename}", layer=crs)
+    except (DataSourceError, DataLayerError):
+        print(f"ERROR: {crs}")
+        return
+    financial_year = [i for i in gf.columns if i[:2] == "20"]
+    print(crs)
+    for year in financial_year:
+        filepath = get_filepath(image, crs, year)
         if os.path.isfile(filepath):
-            print(f"{crs} found")
-            continue
-        try:
-            gf = read_dataframe(f"output/{filename}", layer=crs)
-        except (DataSourceError, DataLayerError):
-            print(f"ERROR: {crs}")
+            print(f"{crs} {year} found")
             continue
         if (gf.type != "LineString").all():
             continue
-        print(crs)
         fig, ax = plt.subplots(dpi=300.0, layout="constrained")
         fig.set_figheight(8.0)
         fig.patch.set_facecolor("#d4ebf2")
         mainland.plot(ax=ax, color="white")
         ax.axis("off")
-        gf["lw"] = get_linear_lw(gf["journeys"])
-        ax.set_title(crs, y=1.0, x=0.0, pad=-12)
+        gf["lw"] = get_linear_lw(gf[year])
+        ax.set_title(f"{crs} {year[:4]}-{year[6:]}", y=1.0, x=0.0, pad=-12)
         gf.plot(ax=ax, linewidth=gf["lw"], color="orange")
         plt.savefig(filepath, bbox_inches="tight")
         plt.close()
+
+
+def main():
+    """main: execution block"""
+    global mainland
+    mainland = read_dataframe("data/ine.gpkg", layer="mainland")
+    pool = Pool(processes=(4 * os.cpu_count() - 1))
+    output_image = partial(write_image)
+    r = pool.map(output_image, sorted(os.listdir("output")))
+    output_image = partial(write_image, image="vector")
+    r = pool.map(output_image, sorted(os.listdir("output")))
 
 
 if __name__ == "__main__":
